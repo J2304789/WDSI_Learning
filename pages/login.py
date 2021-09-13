5c84e179fc789802146742044514b3fd4eb23755
m-oxu <dairenkonmajime@gmail.com>
2021-09-12 21:05:39 -0300

    testing

--- pages/login.py
+++ pages/login.py
@@ -8,6 +8,7 @@ def app():
         password = st.text_input('Password', type='password')
         submit_button = st.form_submit_button(label='Login')
 
-
+    if username == 'moxu':
+        st.succes('luck')
     if __name__ == '__app__':
         app()
