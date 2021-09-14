119e01f951a23c75096048bfc32524a110f761a7
ANVRRT <albertonavarreteramirez@gmail.com>
2021-09-14 02:24:40 -0500

    Finished account creating process, still missing reverting queries if error

--- SQL/UserData.sql
+++ SQL/UserData.sql
@@ -1,6 +1,6 @@
 CREATE TABLE UserData( usernameSpecialHash VARCHAR(70) NOT NULL,
-                       privateKeyEncrypted VARCHAR(100) NOT NULL,
-                       nameEncrypted       VARCHAR(100) NOT NULL,
+                       privateKeyEncrypted VARCHAR(200) NOT NULL,
+                       nameEncrypted       VARCHAR(200) NOT NULL,
                        sex                 CHAR(1)   NOT NULL,
                        age                 INT      NOT NULL,
                        balances            VARCHAR(60000) NOT NULL,
--- SQL/UserLoginData.sql
+++ SQL/UserLoginData.sql
@@ -1,5 +1,5 @@
 CREATE TABLE UserLoginData( hashedUsername  VARCHAR(70)  NOT NULL,
-                            hashedPassword  VARCHAR(20)  NOT NULL,
+                            hashedPassword  VARCHAR(100)  NOT NULL,
                             encryptionKey   VARCHAR(100) NOT NULL,
                             PRIMARY KEY(hashedUsername)
 );
