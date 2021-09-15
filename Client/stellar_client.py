24f87cb7ea3db7f8730b46edf9399ef085159cf2
ANVRRT <albertonavarreteramirez@gmail.com>
2021-09-14 22:43:30 -0500

    Finished Login and Registration services

--- Client/stellar_client.py
+++ Client/stellar_client.py
@@ -1,13 +1,13 @@
 from re import U
-# from stellar_sdk.keypair import Keypair
-# #stellar_sdk.server used to connect to Stellar-core through the Horizon API
-# from stellar_sdk.server import Server
-# #stellar_sdk.network is used to get the Passphrase of either the test or main Stellar network 
-# from stellar_sdk.network import Network
-# #TransactionBuilder is used to create 
-# from stellar_sdk import TransactionBuilder
-# #stellar_sdk.exceptions is used for Error Dectection in transactions
-# from stellar_sdk.exceptions import NotFoundError,BadResponseError,BadRequestError
+from stellar_sdk.keypair import Keypair
+#stellar_sdk.server used to connect to Stellar-core through the Horizon API
+from stellar_sdk.server import Server
+#stellar_sdk.network is used to get the Passphrase of either the test or main Stellar network 
+from stellar_sdk.network import Network
+#TransactionBuilder is used to create 
+from stellar_sdk import TransactionBuilder
+#stellar_sdk.exceptions is used for Error Dectection in transactions
+from stellar_sdk.exceptions import NotFoundError,BadResponseError,BadRequestError
 # #requests is used to initiate the transaction between friendbot and the Public keys
 import requests
 
@@ -23,6 +23,15 @@ from cryptography.fernet import Fernet
 class StellarClient:
 
     def __init__(self):
+
+        self.sessionID = ""
+        self.hashedSessionID = ""
+        self.sessionPassword = ""
+        self.encryptKey = ""
+        self.sex = ""
+        self.age = ""
+        self.balances = ""
+
         self.mySQLConfig = {
                         'user': 'sql5436993',
                         'password': 'v7F7jRWVFc',
@@ -30,7 +39,8 @@ class StellarClient:
                         'database': 'sql5436993',
                         'raise_on_warnings': True
                         }
-        pass
+        
+
 
     def SQL_initialize(self):
         self.connection = mysql.connector.connect(**self.mySQLConfig)
@@ -73,7 +83,7 @@ class StellarClient:
             
             statement.close()
         except Exception as e:
-            # InputManager.display_message(f"Error: {e}")
+            InputManager.display_message(f"Error: {e}")
             data = "Error in query"
         return data
             
@@ -89,7 +99,7 @@ class StellarClient:
             profileID = InputManager.define_string("Please enter your new account ID")              #Enters the new account ID.
             hashedProfileID = sha256(profileID.encode()).hexdigest()                                #Hashes it and turns it into hex string
 
-            query = f"SELECT EXISTS(SELECT * FROM UserLoginData WHERE hashedUsername = '{hashedProfileID}') "       #Prepares SQL query
+            query = f"SELECT EXISTS(SELECT * FROM UserLoginData WHERE hashedUsername = \"{hashedProfileID}\") "       #Prepares SQL query
 
             queryResult = self.SQL_execute_twoway_statement(query)[0][0]                                            #Because the function returns an array with tuples, we want the first registry and the first element from the tuple.
 
@@ -123,9 +133,9 @@ class StellarClient:
 
         encryptKey = Fernet.generate_key()  #Generates a unique encryption key
         fernetKey = Fernet(encryptKey)      #Generates a Fernet object for encrypting things with the key in bytes format
-        encryptKey = Fernet.generate_key().decode()     #Passes the key to a string
+        encryptKey = encryptKey.decode()     #Passes the key to a string
 
-        #QUERIES FOR UserLoginData TABLE
+        #STARTS QUERIES FOR UserLoginData TABLE
 
         query = f"INSERT INTO UserLoginData VALUES(\"{profileData['HashedUsername']}\" , \"{profileData['Password']}\", \"{encryptKey}\")"  #Prepares the SQL query
         queryResult = self.SQL_execute_oneway_statement(query)                                                                              #Executes the SQL query
@@ -134,7 +144,8 @@ class StellarClient:
             InputManager.display_message("There was an error while creating your account, query 1")
             return
 
-        #QUERIES FOR UserData TABLE
+        #FINISHES QUERIES FOR UserLoginData TABLE
+
         usernameSpecialHash = profileData["Username"]+"WDSI"                                                    #We salt the username with a 'WSDI' string for more security
         usernameSpecialHash = sha256(usernameSpecialHash.encode()).hexdigest()                                  #We hash the username
 
@@ -146,6 +157,8 @@ class StellarClient:
 
         balances = "{}"
 
+        #STARTS QUERIES FOR UserData TABLE
+
         query2 = f"INSERT INTO UserData VALUES(\"{usernameSpecialHash}\",\"{encryptedPrivateKey}\",\"{encryptedName}\", \"{profileData['Sex']}\", {profileData['Age']},\"{balances}\")" #Prepares SQL query
         queryResult2 = self.SQL_execute_oneway_statement(query2)
 
@@ -153,16 +166,108 @@ class StellarClient:
             InputManager.display_message("There was an error while creating your account, query 2")
             return
 
-        #QUERIES FOR UserPublicKey TABLE
+        #FINISHES QUERIES FOR UserData TABLE
+
+
+        #STARTS QUERIES FOR UserPublicKey TABLE
     
         query3 = f"INSERT INTO UserPublicKey VALUES(\"{profileData['Username']}\",\"{profileData['PublicKey']}\")"
         queryResult3 = self.SQL_execute_oneway_statement(query3)
 
+        #FINISHES QUERIES FOR UserPublicKey TABLE
+
         if not queryResult3:
-            InputManager.display_message("There was an error while creating your account, query 2")
+            InputManager.display_message(message = "There was an error while creating your account, query 2")
             return
 
-        InputManager.display_message("Account succesfully created")                                         #If no errors, display successful creation
+        InputManager.display_message(message = f"Encrypt Key: {encryptKey}")                                         #If no errors, display successful creation
+        InputManager.display_message(message = "Account succesfully created")                                         #If no errors, display successful creation
+
+    def log_in(self):
+        sessionID = InputManager.define_string("Please enter your account ID")
+        hashedSessionID = sha256(sessionID.encode()).hexdigest()
+
+        sessionPassword = sha256(InputManager.define_string(message="Please enter your password:", infLimit=6, supLimit=30).encode()).hexdigest()
+
+        #STARTS QUERIES FOR UserLoginData TABLE
+
+        query = f"SELECT encryptionKey FROM UserLoginData WHERE hashedUsername = \"{hashedSessionID}\" AND hashedPassword = \"{sessionPassword}\""
+        queryResult = self.SQL_execute_twoway_statement(query)
+        
+        #FINISHES QUERIES FOR UserLoginData TABLE
+
+        if not queryResult:
+            InputManager.display_message(message = "The password or user for this account is incorrect, please try again")
+            return False
+        else:
+            self.sessionID = sessionID
+            self.hashedSessionID = hashedSessionID
+            self.sessionPassword = sessionPassword
+            self.encryptKey = queryResult[0][0].encode()
+
+        usernameSpecialHash = self.sessionID+"WDSI"                                                    #We salt the username with a 'WSDI' string for more security
+        usernameSpecialHash = sha256(usernameSpecialHash.encode()).hexdigest()
+
+        #STARTS QUERIES FOR UserData TABLE
+
+        query2 = f"SELECT * FROM UserData WHERE usernameSpecialHash = \"{usernameSpecialHash}\""
+        queryResult2 = self.SQL_execute_twoway_statement(query2)
+
+        #FINISHES QUERIES FOR UserData TABLE
+
+        fernetKey = Fernet(self.encryptKey)      #Generates a Fernet object for encrypting things with the key in bytes format
+
+        nameEncrypted = queryResult2[0][2].encode()
+        self.name = fernetKey.decrypt(nameEncrypted).decode()
+
+        privateKeyEncrypted = queryResult2[0][1].encode()
+        self.privateKey = fernetKey.decrypt(privateKeyEncrypted).decode()
+
+        self.sex = queryResult2[0][3]
+        self.age = queryResult2[0][4]
+        self.balances = queryResult2[0][5]
+
+        return True
+
+    def main_menu(self):                                                                                    #<---------- Main menu for logged users ---------->
+        def display_balance():                                                                              #Displays the balance of the current logged user.
+            InputManager.display_message(message=f"NOT CODED YET")
+
+        def display_account_data():                                                                         #Displays all the available data of the current logged user.
+            InputManager.display_message(message=f"NOT CODED YET")
+
+        def validate_balance(amount):                                                                       #<---------- Validates if the current balance of the logged user is sufficient for amount sent as parameter.
+            InputManager.display_message(message=f"NOT CODED YET")
+
+
+        def send_payment():                                                                                 #Initializes the process for sending a payment to another user.
+            InputManager.display_message(message=f"NOT CODED YET")
+
+        while True:                                                             #Menu for logged users.
+            print("")
+            print("*****************************************************")
+            print(f"WELCOME {self.sessionID}")
+            print("")
+            print("1) See current balance")
+            print("2) Send payment")
+            print("3) See my account data")
+            print("4) Exit")
+            print("")
+            print("*****************************************************")
+            selectedOption = InputManager.define_numbers(message="Type a number according to your selected option:", infLimit = 1, supLimit = 4,typeOfNumber = int)
+            if selectedOption == 4:
+                break
+            
+            if selectedOption == 1:
+                display_balance()
+            
+            if selectedOption == 2:
+                send_payment()
+
+            if selectedOption == 3:
+                display_account_data()
+        
+
 
 
     def initialize_client(self):
@@ -185,9 +290,9 @@ class StellarClient:
                 break                                                               #Breaks the main loop and exits the program.
             if selectedOption == 1: #Not coded yet                                  #Executes what you need to log in into your account.
                 pass
-                # logFlag = self.log_in()                                             #Calls the log in service, if is successful returns 'True' else returns 'False'.
-                # if logFlag:                                                         #If the log in service is successful, call the main logged menu.
-                    # self.main_menu()
+                logFlag = self.log_in()                                             #Calls the log in service, if is successful returns 'True' else returns 'False'.
+                if logFlag:                                                         #If the log in service is successful, call the main logged menu.
+                    self.main_menu()
 
             if selectedOption == 2:                                                 #Executes the necessary for creating an account.
                 self.create_account()         
